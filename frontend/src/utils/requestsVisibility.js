de4c8a61629345d29824061df8218acb211e7ea9
waraqaweb <waraqaweb@gmail.com>
2026-02-14 00:57:52 +0200

    resetting some changes

---
+++ frontend/src/utils/requestsVisibility.js
@@ -0,0 +1,5 @@
+export const REQUESTS_VISIBILITY_OPTIONS = [
+  { value: 'admin_only', label: 'Admins only' },
+  { value: 'admin_teacher', label: 'Admins and teachers' },
+  { value: 'all_users', label: 'All users' },
+];
